14d6633e854b0ac77bcb9ef7ecbc2d378932771c
Peter Frescura <frescura.peter@gmail.com>
2020-01-13 07:04:36 +0200

    fix docker user and add color

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+# ----------------------------------
+# Colors
+# ----------------------------------
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
 # curl https://raw.githubusercontent.com/puzzledvacuum/devops-box/master/install.sh | sudo bash
 
 set -x # for debug
@@ -39,7 +59,7 @@ fi
 # start docker and enable it:
 systemctl start docker && systemctl enable docker
 # add docker privileges
-echo "Run to add docker user: sudo usermod -G docker ${DOCKER_USER}"
+echo -e "${GREEN}Run to add docker user: sudo usermod -G docker ${USER}${NOCOLOR}"
 # install pip
 pip install -U pip && pip3 install -U pip
 if [[ $? == 127 ]]; then
